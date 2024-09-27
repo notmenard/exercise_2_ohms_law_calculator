c92415ea239c75caf6cbc570175a1041655eb002
Menard Sabugo <menardsabugo@gmail.com>
2024-09-27 12:11:44 +0800

    displaying calculation options

--- exercise_2_ohms_law_calculator.py
+++ exercise_2_ohms_law_calculator.py
@@ -2,6 +2,10 @@
 print(" Ohm's Law Calculator ".center(50, "-"))
 
 # Display calculation options for user.
+print("\nV for Voltage")
+print("I for Current")
+print("R for Resistance")
+
 # Taking user's selection for what they want to calculate
 # If the user chooses to calculate Voltage (V)
 # If the user chooses to calculate Current (I)
