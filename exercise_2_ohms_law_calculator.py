f1b0de750d6fe53081b7ee41a45b89696dfd7ade
Menard Sabugo <menardsabugo@gmail.com>
2024-09-27 12:13:12 +0800

    getting user's input

--- exercise_2_ohms_law_calculator.py
+++ exercise_2_ohms_law_calculator.py
@@ -7,6 +7,8 @@ print("I for Current")
 print("R for Resistance")
 
 # Taking user's selection for what they want to calculate
+selection = str(input("\n Please select what you want to calculate (V, I, or R): ").capitalize())
+
 # If the user chooses to calculate Voltage (V)
 # If the user chooses to calculate Current (I)
 # If the user chooses to calculate Resistance (R)
