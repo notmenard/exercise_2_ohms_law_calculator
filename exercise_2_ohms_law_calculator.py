e951b5b2e0d92a6c6d13e8d29e68a495a7253057
Menard Sabugo <menardsabugo@gmail.com>
2024-09-27 12:09:05 +0800

    pseudocode

---
+++ exercise_2_ohms_law_calculator.py
@@ -0,0 +1,7 @@
+# Add introduction header centered with dashes
+# Display calculation options for user.
+# Taking user's selection for what they want to calculate
+# If the user chooses to calculate Voltage (V)
+# If the user chooses to calculate Current (I)
+# If the user chooses to calculate Resistance (R)
+# Handling invalid user input
